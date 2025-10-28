74ba8e09f202aa8a61ecf465e616e9caa81b13ce
kassymovzhangir <zhangirkassymov2006@gmail.com>
2025-10-28 10:13:34 +0500

    Add files via upload

---
+++ cahnce.js
@@ -0,0 +1,14 @@
+function download(){
+    const link = document.createElement('a');
+    link.download = "C:\Users\Жангир\Downloads\Михаил Круг - Владимирский Централ.mp3";
+    link.href="Михаил Круг - Владимирский Централ.mp3";
+    link.click();
+}
+
+
+function downloado(){
+    const linke = document.createEvent('a');
+    linke.download = "C:\Users\Жангир\Downloads\Бутырка - Шарик.mp3";
+    linke.href="Бутырка - Шарик.mp3";
+    linke.click()
+}
---
+++ disco.js
@@ -0,0 +1,6 @@
+function link(){
+    const link = document.createElement('a');
+    link.href = "The Beach Boys - California Dreamin'.mp3";
+    link.download = "The Beach Boys - California Dreamin'.mp3";
+    link.click();
+}
